f98c91074c56a1f7faf29e52acd2f095161e4d40
McCue Nguyen <mccue.nguyen@gmail.com>
2023-05-20 19:39:03 -0700

    Sructured javascript code

---
+++ Assets/Script/script.js
@@ -0,0 +1,38 @@
+// Var for gathering html elemts
+
+
+//var for quiz questions
+
+
+//Global Var
+
+
+// Function to generate question and answers that cycle through the array
+
+
+//Start quiz and timer, hides startup, and display first question
+
+
+//Function for the end page for when the quiz finishes or timer ends
+
+
+//Sumbit button listen to run function that saves score into the array and to make save it to local storage
+//then to run function that shows updated score menu
+
+
+//Clears scores and redo score menu from local storage
+
+
+//function to hide score menu
+
+
+//clear score funtion from button
+
+
+//replay function from button
+
+
+// Function to check answer if they are wrong or right and displays an alert
+
+
+//start button listener and run quiz 
